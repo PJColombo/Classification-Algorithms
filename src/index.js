91225ba9fc102e35ebf0d27f09d2c321a54f3174
PJColombo <paulojcd2@outlook.com>
2019-04-16 10:15:52 +0200

    setting dependencies configuration

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import 'bootstrap';
+import $ from "jquery";
+
+import './scss/app.scss';
+import './css/style.css';
+
+$(document).ready(() => {
+    console.log("it works");
+});
---
+++ webpack.config.js
@@ -0,0 +1,71 @@
+const path = require('path');
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.JS$/,
+                exclude: /node_modules/,
+                loader: ['babel-loader'],
+            },
+            {
+                test: /\.(scss)$/,
+                use: [
+                    {
+                        // Adds CSS to the DOM by injecting a `<style>` tag
+                        loader: 'style-loader'
+                    },
+                    {
+                        // Interprets `@import` and `url()` like `import/require()` and will resolve them
+                        loader: 'css-loader'
+                    },
+                    {
+                        // Loader for webpack to process CSS with PostCSS
+                        loader: 'postcss-loader',
+                        options: {
+                            plugins: function () {
+                                return [
+                                    require('autoprefixer')
+                                ];
+                            }
+                        }
+                    },
+                    {
+                        // Loads a SASS/SCSS file and compiles it to CSS
+                        loader: 'sass-loader'
+                    }
+                ]
+            },
+            {
+                test: /\.(png|jp(e*)g|svg)$/,
+                use: [{
+                    loader: 'url-loader',
+                    options: {
+                        limit: 8000, // Convert images < 8kb to base64 strings
+                        name: 'images/[hash]-[name].[ext]'
+                    }
+                }]
+            },
+            {
+                test: /\.css$/,
+                exclude: /node_modules/,
+                use: [
+                    { loader: "style-loader" },
+                    { loader: "css-loader" }
+                ]
+            },
+        ]
+    },
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        port: 9000,
+
+    },
+    resolve: {
+        extensions: [".js", ".es6"]
+    }
+};
